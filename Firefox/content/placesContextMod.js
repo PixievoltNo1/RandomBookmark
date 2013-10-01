b4fb3c6baf8a30aeda81fc3dddc269501ee1c60e
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2013-10-01 02:22:18 -0700

    Implemented the main functionality and headed off some bugs

--- Firefox/content/placesContextMod.js
+++ Firefox/content/placesContextMod.js
@@ -24,22 +24,43 @@ if (searchIn != "any") {
 	var menuItem1 = document.createElement("menuitem");
 	menuItem1.id = "RandomBookmarkFromFolder1";
 	menuItem1.setAttribute("label", l10n.get("menuItemFolderOnly"));
-	menuItem1.addEventListener("command", fulfillPurpose.bind("folder"));
+	menuItem1.addEventListener("command", fulfillPurpose.bind(window, "folder"));
 	finalPreparation(menuItem1);
 	var menuItem2 = document.createElement("menuitem");
 	menuItem2.id = "RandomBookmarkFromFolder2";
 	menuItem2.setAttribute("label", l10n.get("menuItemFolderAndSubfolders"));
-	menuItem2.addEventListener("command", fulfillPurpose.bind("folderAndSubfolders"));
+	menuItem2.addEventListener("command", fulfillPurpose.bind(window, "folderAndSubfolders"));
 	finalPreparation(menuItem2);
 }
 function finalPreparation(menuItem) {
 	menuItem.setAttribute("selectiontype", "single");
 	menuItem.setAttribute("selection", "folder");
+	menuItem.setAttribute("forcehideselection", "livemark/feedURI");
 	document.getElementById("placesContext").appendChild(menuItem);
 	cleanupTasks.push( removalTask(menuItem) );
 }
 
 function fulfillPurpose(searchSpace, event) {
+	var folder = PlacesUIUtils.getViewForNode(document.popupNode).selectedNode
+		.QueryInterface(Components.interfaces.nsINavHistoryContainerResultNode);
+	var bookmarks = getBookmarks(folder, searchSpace);
+	var chosen = bookmarks[ Math.floor( Math.random() * bookmarks.length ) ];
+	openUILinkIn(chosen.uri, whereToOpenLink(event))
+}
+function getBookmarks(from, searchSpace) {
+	var bookmarks = [];
+	var oldOpenness = from.containerOpen;
+	from.containerOpen = true;
+	for (let i = 0; i < from.childCount; ++i) {
+		let item = from.getChild(i);
+		if (item.type == item.RESULT_TYPE_URI) {
+			bookmarks.push(item);
+		} else if (item.type == item.RESULT_TYPE_FOLDER && searchSpace == "folderAndSubfolders") {
+			bookmarks = bookmarks.concat( getBookmarks(item, searchSpace) );
+		}
+	}
+	from.containerOpen = oldOpenness;
+	return bookmarks;
 }
 
 extPrefs.addObserver("", { observe: function() {
