c3a10bcebe84858102b4ae35178c039867e6d6c8
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2018-01-24 19:11:02 -0800

    Don't create DOM nodes for folders not being shown

--- WebExtension/ui.module.js
+++ WebExtension/ui.module.js
@@ -1,16 +1,29 @@
 if (!(window.chrome && chrome.runtime)) { window.chrome = browser; }
 import chooseBookmark from './bookmarkSelection.js';
-import folderNode from './svelteComponents/folderNode.html';
+import FolderNodeList from './svelteComponents/FolderNodeList.html';
 import { Store } from 'svelte/store';
 
-chrome.bookmarks.getTree((tree) => { makeFolderList(tree[0], document.getElementById("mainList")); });
-var store = new Store({
-	// TODO: Get user preferences
-	searchIn: "folderOnly",
-	showAndSubfolders: true,
-	l10n_andSubfolders: chrome.i18n.getMessage("andSubfolders"),
+Promise.all([
+	new Promise((resolve) => { chrome.bookmarks.getTree(([tree]) => { resolve(tree); }); }),
+	// TODO: Get user prefs
+]).then(([tree, prefs]) => {
+	var store = new Store({
+		// TODO: Use prefs
+		searchIn: "folderOnly",
+		showAndSubfolders: true,
+		l10n_andSubfolders: chrome.i18n.getMessage("andSubfolders"),
+	});
+	var folders = new FolderNodeList({
+		target: document.getElementById("mainList"),
+		data: {list: makeFolderList(tree).list},
+		store
+	});
+	folders.on("chosen", ({node, andSubfolders}) => {
+		var bookmark = chooseBookmark(node, andSubfolders);
+		window.open(bookmark.url);
+	});
 });
-function makeFolderList(tree, menu) {
+function makeFolderList(tree) {
 	var list = [], hasChildBookmarks = false, hasDescendantBookmarks = false;
 	for (let bookmarkNode of tree.children) {
 		if (bookmarkNode.title == "") {
@@ -27,24 +40,12 @@ function makeFolderList(tree, menu) {
 			if (bookmarkNode.url) { hasChildBookmarks = hasDescendantBookmarks = true; }
 			continue;
 		}
-		let subfolderMenu = document.createDocumentFragment();
-		let folderData = makeFolderList(bookmarkNode, subfolderMenu);
+		let folderData = Object.assign(makeFolderList(bookmarkNode), {node: bookmarkNode});
+		// TODO: Check for bookmarks menu/toolbar folders and set subfolderOpen: true
 		list.push(folderData);
 		if (folderData.hasDescendantBookmarks) {
 			hasDescendantBookmarks = true;
 		}
-		let uiNode = new folderNode({
-			target: menu,
-			data: Object.assign(folderData, {node: bookmarkNode}),
-			store,
-		});
-		if (folderData.list.length) {
-			uiNode.refs.subfolders.appendChild(subfolderMenu);
-		}
-		uiNode.on("chosen", ({node, andSubfolders}) => {
-			var bookmark = chooseBookmark(node, andSubfolders);
-			window.open(bookmark.url);
-		});
 	}
 	while (list.length && list[list.length - 1].separator) {
 		list.pop();
