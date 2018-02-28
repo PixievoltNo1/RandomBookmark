a885e83708d3fd56b0fe4b0680bea902205c25ab
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2018-02-28 03:19:19 -0800

    Just use a pre-rasterized 32x32 image for the browser action

--- WebExtension/background.js
+++ WebExtension/background.js
@@ -1,10 +1,5 @@
 if (!(window.chrome && chrome.runtime)) { window.chrome = browser; }
 
-// Workaround for Chrome only supporting SVG files when they're specified by setIcon
-chrome.browserAction.setIcon({path:
-	chrome.runtime.getManifest().browser_action.default_icon
-});
-
 chrome.runtime.onInstalled.addListener(async function({reason}) {
 	if (reason != "update") { return; }
 	var {searchIn: oldSearchIn = false} = await new Promise(
