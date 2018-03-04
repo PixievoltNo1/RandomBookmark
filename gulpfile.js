2ba68914674ce5a2ee4c56dc3f4d4e16d9529f3b
Pikadude No. 1 <pikadudeno1+git@gmail.com>
2018-03-04 04:21:33 -0800

    Build tools now use a build folder + added a makeRelease script for creating a release-ready folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,44 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var fileUrl = require('file-url');
+var del = require('del');
 
 var sassSrc = ["./WebExtension/**/*.scss"];
 function compileSass() {
 	return gulp.src(sassSrc, {base: "./WebExtension/"})
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.write('./sourcemaps/', {
+		.pipe(sourcemaps.write('./', {
 			includeContent: false,
-			sourceMappingURLPrefix: fileUrl("WebExtension"),
-			destPath: "./WebExtension/"
+			sourceMappingURLPrefix: fileUrl("WebExtension/build"),
+			destPath: "./WebExtension/build/"
 		}))
-		.pipe(gulp.dest("./WebExtension/"));
+		.pipe(gulp.dest("./WebExtension/build/"));
 }
 
 gulp.task("build", gulp.parallel(compileSass));
 gulp.task("watch", gulp.series("build", function watchSass() {
 	gulp.watch(sassSrc, compileSass);
 }));
+gulp.task("makeRelease", gulp.series(
+	function delRelease() {
+		return del("release");
+	},
+	gulp.parallel(
+		function copyNonBuilt() {
+			var srcForWebpack = [
+				"./WebExtension/**/*.module.js",
+				"./WebExtension/**/svelteComponents{,/**/*}"
+			];
+			var exclusions = ["./WebExtension/build/**/*", ...sassSrc, ...srcForWebpack]
+				.map((excludeMe) => { return "!" + excludeMe; });
+			return gulp.src(["./WebExtension/**/*", ...exclusions])
+				.pipe(gulp.dest("./release/"));
+		},
+		function compileSassForRelease() {
+			return gulp.src(sassSrc)
+				.pipe(sass())
+				.pipe(gulp.dest("./release/build/"));
+		}
+	)
+));
--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ var path = require('path');
 var fileUrl = require('file-url');
 
 // TODO: Write alternate configuration for extension store releases
-module.exports = {
+module.exports = function(env = {}) { return {
 	entry: {
 		ui: './WebExtension/ui.module.js',
 		options: './WebExtension/options.module.js'
 	},
 	output: {
-		path: path.resolve(__dirname, 'WebExtension'),
-		filename: '[name].bundle.js'
+		path: path.resolve(__dirname, env.release ? 'release/build' : 'WebExtension/build'),
+		filename: '[name].js'
 	},
 	// Firefox & Chrome don't correctly handle relative URLs for source maps in extensions. Workaround is in plugins.
 	// devtool: "source-maps",
@@ -33,12 +33,14 @@ module.exports = {
 			name: "common",
 		}),
 		// Workaround for above Firefox/Chrome issue
-		new webpack.SourceMapDevToolPlugin({
-			publicPath: fileUrl("WebExtension") + "/",
-			filename: "sourcemaps/[file].map"
-		})
+		...( env.release ? [] : [
+			new webpack.SourceMapDevToolPlugin({
+				publicPath: fileUrl("WebExtension/build") + "/",
+				filename: "[file].map"
+			}),
+		] )
 	],
 	stats: {
 		optimizationBailout: true,
 	},
-};
+}; };
